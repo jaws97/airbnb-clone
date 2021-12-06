0fdb7709bdf661b17aacf706dc0dd49b2d6a0ae6
Michael, Arokia <aromichael@deloitte.com>
2021-12-06 15:48:40 +0530

    landing page created

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,69 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
 
-export default function Home() {
+export default function Home({exploreData, cardsData}) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Arokia's Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Let's build Air BNB</h1>
+      
+      <Header />
+      <Banner />
+
+      <main className="max-w-5xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item,index)=>(
+              <SmallCard img={item.img}
+              key={index}
+              distance={item.distance}
+              location={item.location} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-x-scroll overflow-y-hidden scrollbar-hide
+          p-3 -ml-3">
+            {cardsData?.map((card)=>(
+              <MediumCard key={card.img}
+              img={card.img}
+              title={card.title} />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
+      </main>
     </div>
   )
+}
+
+
+export async function getStaticProps(){
+  const exploreData = await fetch('https://links.papareact.com/pyp').
+  then(res => res.json());
+
+  const cardsData = await fetch("https://links.papareact.com/zp1").
+  then((res)=> res.json());
+  console.log(cardsData)
+  return {
+    props : {
+      exploreData,
+      cardsData
+    } 
+  }
 }
