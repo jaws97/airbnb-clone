7f80797a977ba54be26fa609c270607ada57c76e
Michael, Arokia <aromichael@deloitte.com>
2021-12-10 10:04:15 +0530

    added footer in home page

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer(){
+    return(
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 px-32
+        py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referrals accepted</p>
+                <p>Arokia's Clone</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Arokia Lijas</p>
+                <p>Presents</p>
+                <p>NextJs Application</p>
+                <p>...</p>
+                <p>View All</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Centre</p>
+                <p>Trust & Safety</p>
+                <p>Say Hi Youtube</p>
+                <p>Easter Eggs</p>
+                <p>For the win</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
